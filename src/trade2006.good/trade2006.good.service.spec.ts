bab7e29878316eb65bccf4adf9e79d732da10289
Vladimir Lastovka <elcopro@gmail.com>
2023-06-02 16:24:39 +0300

    invoice

--- src/trade2006.good/trade2006.good.service.spec.ts
+++ src/trade2006.good/trade2006.good.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Trade2006GoodService } from './trade2006.good.service';
+import { FIREBIRD } from '../firebird/firebird.module';
 
 describe('Trade2006GoodService', () => {
     let service: Trade2006GoodService;
+    const query = jest.fn().mockReturnValue([{ GOODSCODE: 1, QUAN: 2 }]);
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [Trade2006GoodService],
+            providers: [Trade2006GoodService, { provide: FIREBIRD, useValue: { query } }],
         }).compile();
 
         service = module.get<Trade2006GoodService>(Trade2006GoodService);
@@ -14,5 +16,14 @@ describe('Trade2006GoodService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('test in', async () => {
+        const res = await service.in(['1']);
+        expect(res).toEqual([{ code: 1, quantity: 2 }]);
+        expect(query.mock.calls[0]).toEqual([
+            'SELECT GOODS.GOODSCODE, SHOPSKLAD.QUAN  FROM GOODS JOIN SHOPSKLAD ON GOODS.GOODSCODE = SHOPSKLAD.GOODSCODE WHERE GOODS.GOODSCODE IN (?)',
+            ['1'],
+        ]);
     });
 });
--- src/trade2006.invoice/trade2006.invoice.service.spec.ts
+++ src/trade2006.invoice/trade2006.invoice.service.spec.ts
@@ -1,12 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Trade2006InvoiceService } from './trade2006.invoice.service';
+import { FIREBIRD } from '../firebird/firebird.module';
+import { ConfigService } from '@nestjs/config';
 
 describe('Trade2006InvoiceService', () => {
     let service: Trade2006InvoiceService;
-
+    const query = jest
+        .fn()
+        .mockResolvedValueOnce([{ MAX: 1 }])
+        .mockResolvedValueOnce([{ GEN_ID: 2 }])
+        .mockRejectedValueOnce({ message: 'Test error' });
+    const execute = jest.fn();
+    const commit = jest.fn();
+    const rollback = jest.fn();
+    const get = jest.fn().mockReturnValueOnce(1).mockReturnValueOnce(2);
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [Trade2006InvoiceService],
+            providers: [
+                Trade2006InvoiceService,
+                {
+                    provide: FIREBIRD,
+                    useValue: {
+                        query: async (query: string, remark: string[]) => (remark[0] === 'test1' ? [1] : []),
+                        transaction: async () => ({
+                            query,
+                            execute,
+                            commit,
+                            rollback,
+                        }),
+                    },
+                },
+                {
+                    provide: ConfigService,
+                    useValue: { get },
+                },
+            ],
         }).compile();
 
         service = module.get<Trade2006InvoiceService>(Trade2006InvoiceService);
@@ -14,5 +42,50 @@ describe('Trade2006InvoiceService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('test create', async () => {
+        const date = new Date();
+        let res = await service.create({
+            buyerId: 1,
+            remark: 'test remark',
+            date,
+            invoiceLines: [
+                {
+                    goodCode: '1',
+                    quantity: 2,
+                    price: '3.33',
+                },
+            ],
+        });
+        expect(res).toBeTruthy();
+        expect(query.mock.calls[0]).toEqual([
+            'SELECT MAX(NS) FROM S WHERE FIRM_ID = ? AND DATA > ?',
+            [1, '2023-01-01'],
+        ]);
+        expect(query.mock.calls[1]).toEqual(['SELECT GEN_ID(SCODE_GEN, 0) from rdb$database', []]);
+        expect(execute.mock.calls).toHaveLength(3);
+        expect(commit.mock.calls).toHaveLength(1);
+        expect(rollback.mock.calls).toHaveLength(0);
+        res = await service.create({
+            buyerId: 1,
+            remark: 'test remark',
+            date,
+            invoiceLines: [
+                {
+                    goodCode: '1',
+                    quantity: 2,
+                    price: '3.33',
+                },
+            ],
+        });
+        expect(res).toBeFalsy();
+        expect(rollback.mock.calls).toHaveLength(1);
+    });
+    it('test isExists', async () => {
+        let res = await service.isExists('test1');
+        expect(res).toBeTruthy();
+        res = await service.isExists('test2');
+        expect(res).toBeFalsy();
     });
 });
