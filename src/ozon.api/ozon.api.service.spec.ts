c344f9b2433c7beaa1f763e8727b8123f8d08f9f
Vladimir Lastovka <elcopro@gmail.com>
2023-06-02 14:48:46 +0300

    product

--- src/ozon.api/ozon.api.service.spec.ts
+++ src/ozon.api/ozon.api.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OzonApiService } from './ozon.api.service';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { of } from 'rxjs';
 
 describe('OzonApiService', () => {
     let service: OzonApiService;
+    const post = jest.fn().mockReturnValue(of('post'));
+    const get = jest.fn().mockReturnValue('get');
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [OzonApiService],
+            providers: [
+                OzonApiService,
+                { provide: HttpService, useValue: { post } },
+                { provide: ConfigService, useValue: { get } },
+            ],
         }).compile();
 
         service = module.get<OzonApiService>(OzonApiService);
@@ -14,5 +23,19 @@ describe('OzonApiService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('test method', async () => {
+        await service.method('test', { option: 'option ' });
+        expect(get.mock.calls).toEqual([
+            ['OZON_URL', 'https://api-seller.ozon.ru'],
+            ['CLIENT-ID', ''],
+            ['API-KEY', ''],
+        ]);
+        expect(post.mock.calls[0]).toEqual([
+            'gettest',
+            { option: 'option ' },
+            { headers: { 'Client-Id': 'get', 'Api-Key': 'get' } },
+        ]);
     });
 });
--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
+import { OzonApiService } from '../ozon.api/ozon.api.service';
 
 describe('ProductService', () => {
     let service: ProductService;
 
+    const method = jest.fn();
+
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [ProductService],
+            providers: [ProductService, { provide: OzonApiService, useValue: { method } }],
         }).compile();
-
+        method.mockClear();
         service = module.get<ProductService>(ProductService);
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    it('test list', async () => {
+        await service.list();
+        expect(method.mock.calls[0]).toEqual(['/v2/product/list', { last_id: '', limit: 100 }]);
+    });
+    it('test updateCount', async () => {
+        await service.updateCount([{ offer_id: '1', product_id: 1, stock: 1 }]);
+        expect(method.mock.calls[0]).toEqual([
+            '/v1/product/import/stocks',
+            { stocks: [{ offer_id: '1', product_id: 1, stock: 1 }] },
+        ]);
+    });
+    it('test listWithCount', async () => {
+        await service.listWithCount();
+        expect(method.mock.calls[0]).toEqual(['/v3/product/info/stocks', { filter: {}, last_id: '', limit: 100 }]);
+    });
+    it('test orderList', async () => {
+        const date = new Date();
+        await service.orderList({ since: date, to: date, status: 'test' });
+        expect(method.mock.calls[0]).toEqual([
+            '/v3/posting/fbs/list',
+            { filter: { since: date, to: date, status: 'test' }, limit: 100 },
+        ]);
     });
 });
