3f9fafc8420a0e7d391df8a98d665e4eafbc1e7d
Vladimir Lastovka <elcopro@gmail.com>
2025-05-24 08:12:47 +0300

    feature: rest tests, vault clear config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { VaultService } from 'vault-module/lib/vault.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private readonly vaultService: VaultService,
+    ) {}
 
     @Get()
     getHello(): string {
         return this.appService.getHello();
+    }
+
+    @Post('vault/clear-cache')
+    async clearVaultCache() {
+        await this.vaultService.clearCache();
+        return { message: 'Vault cache cleared successfully' };
     }
 }
